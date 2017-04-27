eea3f6a9d99eb3f748468688db95f997c830e253
LOGAN HIRSCHI <logan.d@hotmail.com>
2017-04-26 23:07:41 -0600

    same

--- Slade.js
+++ Slade.js
@@ -1,4 +1,5 @@
 var babyPrinceHog = document.getElementById("phil");
+var undertale = document.getElementById("josh");
 document.addEventListener("click", function() {
   if (babyPrinceHog.style.color == "green"){
     babyPrinceHog.style.color = "red";
@@ -6,3 +7,15 @@ document.addEventListener("click", function() {
     babyPrinceHog.style.color = "green";
   }
 })
+if (document.body.style.background = "yellow,"){
+  undertale.style.height = "200px";
+  }else{
+    undertale.style.height = "300px";
+  }
+function undertaleWeird() {
+  if (document.body.style.background == "red"){
+    babyPrinceHog.style.color = "green";
+  }else{
+    babyPrinceHog.style.color = "black";
+  }
+}
