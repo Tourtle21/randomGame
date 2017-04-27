e567a31cbde180bfa9d758d28183181da2d73514
LOGAN HIRSCHI <logan.d@hotmail.com>
2017-04-26 19:40:13 -0600

    sawDust

--- Slade.js
+++ Slade.js
@@ -1,4 +1,8 @@
 var babyPrinceHog = document.getElementById("phil");
 document.addEventListener("click", function() {
-  babyPrinceHog.style.color = "green"
+  if (babyPrinceHog.style.color == "green"){
+    babyPrinceHog.style.color = "red";
+  }else{
+    babyPrinceHog.style.color = "green";
+  }
 })
